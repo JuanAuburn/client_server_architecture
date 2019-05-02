b8c66608504161d67aa4ab5c7cad7644b95412d5
juanauburn <juanauburn@gmail.com>
2019-05-02 09:32:41 -0500

    File server (first part complete)

--- FileServer/client.py
+++ FileServer/client.py
@@ -1,13 +1,15 @@
-import zmq
 import sys
-from utilities import md5_for_file
+
+import zmq
+import utilities
+import os
 
 
 class Client:
     port_server = "9999"
-    chunk_file = 20
     context = None
     socket = None
+    dictionary_files = {}
 
     def __init__(self):
         self.context = zmq.Context()
@@ -18,41 +20,119 @@ class Client:
             self.socket.connect("tcp://localhost:%s" % self.port_server)
 
     def send_message(self, message):
-        self.socket.send(message.encode())
+        self.socket.send_multipart(['send_message'.encode(), message.encode()])
         response = self.socket.recv().decode()
         print(response)
 
     def send_file(self, file_name):
-        hash_file = md5_for_file(file_name)
-        print("hash_file to send: "+hash_file)
-        file = open(file_name, 'r')
+        if os.path.exists(file_name):
+            try:
+                file = open(file_name, 'rb')
+                hash_file = utilities.md5_for_file(file_name)
+                print("hash_file to send: "+file_name)
+                print("chunk file: "+str(utilities.chunk_file))
+                part = 0
+                while True:
+                    data = file.read(utilities.chunk_file)
+                    # print(data)
+                    if not data:
+                        print("no more data")
+                        part = -1
+                        self.socket.send_multipart(['send_file'.encode(), str(hash_file).encode(), str(part).encode(), ''.encode()])
+                        print("server response: " + self.socket.recv_multipart()[0].decode())
+                        break
+                    print("sending the part: "+str(part))
+                    self.socket.send_multipart(['send_file'.encode(), str(hash_file).encode(), str(part).encode(), data])
+                    server_response = self.socket.recv_multipart()
+                    print("server response: " + server_response[0].decode())
+                    part += 1
+                    if server_response[0].decode() != 'OK':
+                        print("ERROR - " + server_response[1].decode())
+                        break
+                print("finish to send the file")
+            except IOError as error:
+                print(error)
+        else:
+            print('The file "' + file_name + '" no exist. Try again')
+
+    def get_file(self, hash_file_name, name_file_to_save):
         part = 0
+        if os.path.exists('files_client/' + name_file_to_save):
+            os.remove('files_client/' + name_file_to_save)
         while True:
-            data = file.read(self.chunk_file)
-            if not data:
-                part = -1
-                self.socket.send_multipart([str(hash_file).encode(), str(part).encode(), ''.encode()])
-                print("server response: " + self.socket.recv_string())
+            self.socket.send_multipart(['get_file'.encode(), hash_file_name.encode(), str(part).encode()])
+            response = self.socket.recv_multipart()
+            method = response[0].decode()
+            if (not response[0]) or method == 'ERROR':
+                print('ERROR to get the file '+hash_file_name+': '+response[1].decode())
                 break
-            print("sending the part: "+str(part))
-            self.socket.send_multipart([str(hash_file).encode(), str(part).encode(), data.encode()])
-            server_response = self.socket.recv_string()
-            print("server response: " + server_response)
+            elif method == 'send_file':
+                hash_file = response[1].decode()
+                part = int(response[2].decode())
+                data = response[3]
+                if hash_file == hash_file_name:
+                    if part == 0:
+                        if not self.dictionary_files.get(hash_file_name):
+                            try:
+                                file = open('files_client/' + name_file_to_save, "ab")
+                                print("create file '" + name_file_to_save + "'")
+                                file.write(data)
+                                self.dictionary_files[hash_file_name] = file
+                            except IOError as error:
+                                print("ERROR - The file '" + hash_file_name + "' its already exist (" + str(error) + ")")
+                                break
+                        else:
+                            print("ERROR - The file '" + hash_file_name + "' its already exist")
+                            break
+                    elif part == -1:
+                        if self.dictionary_files.get(hash_file_name):
+                            self.dictionary_files[hash_file_name].close()
+                            self.dictionary_files.pop(hash_file_name)
+                            print("the file '" + name_file_to_save + "' its complete")
+                            break
+                        else:
+                            print("ERROR - The file to close '" + name_file_to_save + "' no exist")
+                            break
+                    else:
+                        if self.dictionary_files.get(hash_file_name):
+                            file = self.dictionary_files[hash_file_name]
+                            print(name_file_to_save + " - receiving the part " + str(part))
+                            file.write(data)
+                        else:
+                            print("ERROR - The file '" + name_file_to_save + "' to write the part " + str(part) + " no exist")
+                else:
+                    print("The file received '"+hash_file+"' is different to the wanted '"+hash_file_name+"'")
+                    break
+            else:
+                print("The method '"+method+"' no is valid")
+                break
             part += 1
-            if server_response != 'ok':
-                break
-        print("finish to send the file")
 
 
 if __name__ == '__main__':
     client = Client()
     while True:
-        print("press any key to send server file:")
-        input()
-        client.send_file('server.py')
-        print("press any key to send client file:")
-        input()
-        client.send_file('client.py')
-        print("press any key to send utilities file:")
-        input()
-        client.send_file('utilities.py')
+        print("write the file name to send: ")
+        file_name1 = input()
+        if file_name1 != '':
+            client.send_file(file_name1)
+            hash_file1 = ''
+            if os.path.exists(file_name1):
+                hash_file1 = utilities.md5_for_file(file_name1)
+            print("press any key for continue to get the same file: ")
+            input()
+            client.get_file(hash_file1, file_name1)
+        else:
+            print("The file name to send can not be empty. Try again")
+        # print("press any key to get the file: 9694b7c165af81f4d9d372f54117c0f1")
+        # input()
+        # client.get_file_by_hash('9694b7c165af81f4d9d372f54117c0f1')
+        # print("press any key to send server file:")
+        # input()
+        # client.send_file('server.py')
+        # print("press any key to send client file:")
+        # input()
+        # client.send_file('client.py')
+        # print("press any key to send utilities file:")
+        # input()
+        # client.send_file('utilities.py')
--- FileServer/server.py
+++ FileServer/server.py
@@ -1,9 +1,10 @@
 import zmq
+import utilities
+import os
 
 
 class Server:
     port = "9999"
-    chunk_file = 20
     context = None
     socket = None
     count_files = 0
@@ -14,66 +15,100 @@ class Server:
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind("tcp://*:%s" % self.port)
 
-    def read_messages(self):
+    def listen(self):
         while True:
             try:
-                message = self.socket.recv().decode()
-                print("Received message: ", message)
-                self.socket.send_string("receive")
+                message = self.socket.recv_multipart()
+                method = message[0]
+                if method:
+                    method = method.decode()
+                    # print("method: "+method)
+                    if method == 'send_file':
+                        method, hash_file, part, data = message
+                        hash_file = hash_file.decode()
+                        part = int(part.decode())
+                        self.get_file(hash_file, part, data)
+                    elif method == 'get_file':
+                        method, hash_file_name, part = message
+                        hash_file_name = hash_file_name.decode()
+                        part = int(part.decode())
+                        self.send_file(hash_file_name, part)
+                    elif method == 'send_message':
+                        method, message_content = message
+                        message_content = message_content.decode()
+                        self.read_message(message_content)
             except zmq.ZMQError as error:
-                print("Error to read message '" + str(error) + "'")
-
-    def read_files(self):
-        while True:
-            try:
-                hash_file, part, data = self.socket.recv_multipart()
-                hash_file = hash_file.decode()
-                # print("hash_file: "+hash_file)
-                # print("part: "+part.decode())
-                part = int(part.decode())
-                data = data.decode()
-                # print(data)
-                if part == 0:
-                    if not self.dictionary_files.get(hash_file):
-                        try:
-                            file = open('files/' + hash_file, "x")
-                            print("create file '" + hash_file + "'")
-                            file.write(data)
-                            self.dictionary_files[hash_file] = file
-                            self.socket.send_string("ok")
-                        except IOError as error:
-                            print("ERROR - The file '" + hash_file + "' its already exist (" + str(error) + ")")
-                            self.socket.send_string("the file already exist")
-                    else:
-                        print("ERROR - The file '" + hash_file + "' its already exist")
-                        self.socket.send_string("the file already exist")
-                elif part == -1:
-                    if self.dictionary_files.get(hash_file):
-                        self.dictionary_files[hash_file].close()
-                        self.dictionary_files.pop(hash_file)
-                        print("the file '" + hash_file + "' its complete")
-                        self.socket.send_string("ok")
-                    else:
-                        print("ERROR - The file to close '" + hash_file + "' no exist")
-                        self.socket.send_string("the file to finish no exist or already finish")
-                else:
-                    if self.dictionary_files.get(hash_file):
-                        file = self.dictionary_files[hash_file]
-                        print(hash_file + " - receiving the part " + str(part))
-                        file.write(data)
-                        self.socket.send_string("ok")
-                    else:
-                        print("ERROR - The file '" + hash_file + "' to write the part " + str(part) + " no exist")
-                        self.socket.send_string("the file to write no exist or already created")
-            except zmq.ZMQError as error:
-                print("Error to read file '" + str(error) + "'")
+                print("Error to get request '" + str(error) + "'")
                 try:
-                    self.socket.send_string("ERROR")
+                    self.socket.send_multipart(['ERROR'.encode(), ("Error to get request '" + str(error) + "'").encode()])
                 except zmq.ZMQError as error:
                     print("ERROR '" + str(error) + "'")
                 break
 
+    def read_message(self, message):
+        print("Received message: ", message)
+        self.socket.send_multipart(['OK'.encode(), ''.encode()])
+
+    def get_file(self, hash_file, part, data):
+        if part == 0:
+            if os.path.exists('files/' + hash_file):
+                print("ERROR - The file '" + hash_file + "' its already exist")
+                self.socket.send_multipart(['ERROR'.encode(), 'the file already exist'.encode()])
+            else:
+                try:
+                    print("create file '" + hash_file + "'")
+                    file = open('files/' + hash_file, "ab")
+                    file.write(data)
+                    self.dictionary_files[hash_file] = file
+                    self.socket.send_multipart(['OK'.encode(), ''.encode()])
+                except IOError as error:
+                    print("ERROR - The file '" + hash_file + "' its already exist (" + str(error) + ")")
+                    self.socket.send_multipart(['OK'.encode(), 'the file already exist'.encode()])
+        elif part == -1:
+            if self.dictionary_files.get(hash_file):
+                self.dictionary_files[hash_file].close()
+                self.dictionary_files.pop(hash_file)
+                print("the file '" + hash_file + "' its complete")
+                self.socket.send_multipart(['OK'.encode(), ''.encode()])
+            else:
+                print("ERROR - The file to close '" + hash_file + "' no exist")
+                self.socket.send_multipart(['ERROR'.encode(), 'the file to finish no exist or already finish'.encode()])
+        else:
+            if self.dictionary_files.get(hash_file):
+                file = self.dictionary_files[hash_file]
+                print(hash_file + " - receiving the part " + str(part))
+                file.write(data)
+                self.socket.send_multipart(['OK'.encode(), ''.encode()])
+            else:
+                print("ERROR - The file '" + hash_file + "' to write the part " + str(part) + " no exist")
+                self.socket.send_multipart(['ERROR'.encode(), 'the file to write no exist or already created'.encode()])
+
+    def send_file(self, hash_file, part):
+        print("hash_file to send: "+hash_file)
+        if os.path.exists('files/' + hash_file):
+            try:
+                file = open('files/' + hash_file, 'rb')
+                file.read(utilities.chunk_file*part)
+                data = file.read(utilities.chunk_file)
+                if not data:
+                    part = -1
+                    self.socket.send_multipart(['send_file'.encode(), str(hash_file).encode(), str(part).encode(), ''.encode()])
+                    # print("client response: " + self.socket.recv_multipart()[0].decode())
+                else:
+                    print("sending the part: "+str(part))
+                    self.socket.send_multipart(['send_file'.encode(), str(hash_file).encode(), str(part).encode(), data])
+                    # print("client response: " + self.socket.recv_multipart()[0].decode())
+                file.close()
+                print("finish to send the file")
+            except IOError as error:
+                self.socket.send_multipart(['ERROR'.encode(), ('Can´t find the file with the hash: '+hash_file).encode()])
+                print(error)
+        else:
+            print("The file '" + hash_file + "' no exist")
+            self.socket.send_multipart(['ERROR'.encode(), ('Can´t find the file with the hash: ' + hash_file).encode()])
+
 
 if __name__ == '__main__':
     server = Server()
-    server.read_files()
+    print("Server running")
+    server.listen()
